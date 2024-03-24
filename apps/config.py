5060bc9c9de831a01f967d3f279d0034e8245760
blah037 <p.buyantsog1@gmail.com>
2024-03-24 15:22:58 +0800

    5

--- apps/config.py
+++ apps/config.py
@@ -5,27 +5,27 @@ Copyright (c) 2019 - present AppSeed.us
 
 import os, random, string
 
-class Config(object):
 
+class Config(object):
     basedir = os.path.abspath(os.path.dirname(__file__))
 
     # Assets Management
     ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')
 
     # Set up the App SECRET_KEY
-    SECRET_KEY  = os.getenv('SECRET_KEY', None)
+    SECRET_KEY = os.getenv('SECRET_KEY', None)
     if not SECRET_KEY:
-        SECRET_KEY = ''.join(random.choice( string.ascii_lowercase  ) for i in range( 32 ))
+        SECRET_KEY = ''.join(random.choice(string.ascii_lowercase) for i in range(32))
 
     # Social AUTH context
-    SOCIAL_AUTH_GITHUB  = False
+    SOCIAL_AUTH_GITHUB = False
 
-    GITHUB_ID      = os.getenv('GITHUB_ID'    , None)
-    GITHUB_SECRET  = os.getenv('GITHUB_SECRET', None)
+    GITHUB_ID = os.getenv('GITHUB_ID', None)
+    GITHUB_SECRET = os.getenv('GITHUB_SECRET', None)
 
     # Enable/Disable GitHub Social Login
     if GITHUB_ID and GITHUB_SECRET:
-         SOCIAL_AUTH_GITHUB  = True
+        SOCIAL_AUTH_GITHUB = True
 
     import os
 
@@ -33,12 +33,12 @@ class Config(object):
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    DB_ENGINE = os.getenv('DB_ENGINE', 'mysql')
-    DB_USERNAME = os.getenv('DB_USERNAME', 'root')
+    DB_ENGINE = os.getenv('DB_ENGINE', None)  # 'mysql'
+    DB_USERNAME = os.getenv('DB_USERNAME', None)  # 'root'
     DB_PASS = os.getenv('DB_PASS', '89024')
     DB_HOST = os.getenv('DB_HOST', 'localhost')
     DB_PORT = os.getenv('DB_PORT', '3306')
-    DB_NAME = os.getenv('DB_NAME', 'hemny_db')
+    DB_NAME = os.getenv('DB_NAME', None)  # 'hemny_db'
 
     USE_SQLITE = True
 
@@ -74,11 +74,13 @@ class ProductionConfig(Config):
     REMEMBER_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_DURATION = 3600
 
+
 class DebugConfig(Config):
     DEBUG = True
+
 
 # Load all possible configurations
 config_dict = {
     'Production': ProductionConfig,
-    'Debug'     : DebugConfig
+    'Debug': DebugConfig
 }
